ad5d3bf5983c03e0bcd6ad42a4306f776fbfb85e
PokeMy14 <AaronAnehasseRomera@192.168.1.9>
2020-11-10 18:38:19 +0100

    First Sync

---
+++ src/main/java/net/mcreator/boudicasmod/item/SulfurDustItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.boudicasmod.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.boudicasmod.BoudicasModModElements;
+
+@BoudicasModModElements.ModElement.Tag
+public class SulfurDustItem extends BoudicasModModElements.ModElement {
+	@ObjectHolder("boudicas_mod:sulfur_dust")
+	public static final Item block = null;
+	public SulfurDustItem(BoudicasModModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("sulfur_dust");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
